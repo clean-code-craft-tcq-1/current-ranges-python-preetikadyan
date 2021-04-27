d79fcdeb82800bc2b8fb0d6da282e6c6f752c695
PREETI <preetikadyan634@gmail.com>
2021-04-27 20:20:28 +0530

    Update current_range.test.py

--- current_range.test.py
+++ current_range.test.py
@@ -1,9 +1,11 @@
 #Test_current_ranges
 
 import unittest
+import identify_range_and_count_implementation as ir
 
 class CurrentRangeTest(unittest.TestCase):
-  
+    def test_range_and_count(self):
+        ir.identify_range_and_count([3,5,3,10,15,16,17])
   
 
 if __name__ == "__main__":
