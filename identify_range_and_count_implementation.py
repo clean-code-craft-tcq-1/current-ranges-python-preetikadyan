6475f2446fb4f0144a699755a5f4324568201ebf
PREETI <preetikadyan634@gmail.com>
2021-04-28 15:11:40 +0530

    Update identify_range_and_count_implementation.py

--- identify_range_and_count_implementation.py
+++ identify_range_and_count_implementation.py
@@ -2,16 +2,20 @@
 
 def identify_range_and_count(list_of_nos):
     #ar=[3,5,3,10,15]
-    list_of_nos.sort()
+    list_of_nos.sort() #sort the element to ascending order
     count=[]
     strt=[]
     end=[]
     i=0
     ct=-1
-    pre= list_of_nos[0]-5 #to make sure that in the first itteration it doesn't satisfy the if condition and go to else
+    pre= list_of_nos[0]-5 
+    
+    #in this logic I am comparing the current element with the previous element
+    #First element doesn't have a previous value so i.e., why the initial previous contains a negative index
+    #to make sure that in the first itteration it doesn't satisfy the if condition and go to else
     
     while(i<len(list_of_nos)):
-        if ((list_of_nos[i]-pre)<2):
+        if ((list_of_nos[i]-pre)<2): #whenever there is a range the difference of current and previous value is either '0'(when they are equal) or '1'
             count[ct]+=1
             end[ct]=list_of_nos[i]
         else:
@@ -20,7 +24,7 @@ def identify_range_and_count(list_of_nos):
             end.append(list_of_nos[i])
             ct+=1
 
-        pre=list_of_nos[i]
+        pre=list_of_nos[i] #with every itteration current value becomes previous
         i+=1
     
     print("Range, Readings")
